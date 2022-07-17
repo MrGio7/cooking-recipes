743cac6fcaff1190fb4c586f793f720f96e18a74
MrGio7 <lashagiorgi1233@gmail.com>
2022-07-17 18:29:44 +0400

    added ingredients seed

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const ingredients = ["Flour", "Milk", "Oil", "Salt", "Sugar", "Eggs", "Tomatoes", "Peppers", "Cheese", "Potatoes", "Meat"]
+
+async function main() {
+  await prisma.ingredient.createMany({
+    data: ingredients.map((ingredient) => ({ name: ingredient })),
+    skipDuplicates: true
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
